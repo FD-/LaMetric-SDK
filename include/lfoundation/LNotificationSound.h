b98ab5ea6c5e7093f52d299531fffa59b343dd36
FD- <fdraschbacher@gmail.com>
2020-09-28 17:57:04 +0200

    Improved LNotificationSound documentation

--- include/lfoundation/LNotificationSound.h
+++ include/lfoundation/LNotificationSound.h
@@ -18,13 +18,20 @@ class LNotificationSound : public QObject {
     Q_OBJECT
 
 public:
+    /* Apparently, a new LNotificationSound has to be created before an existing one can be deleted! Do something like
+
+        LNotificationSound *newNotificationSound = createFromString("notification", "cash", this);
+        delete notificationSound;
+        notificationSound = newNotificationSound;
+        
+    Otherwise, the new sound won't play.  */
     static LNotificationSound *createFromId(NotificationSoundId id, QObject *parent);
     static LNotificationSound *createFromString(SoundCategory category, const QString &name, QObject *parent);
     static LNotificationSound *createFromString(const QString &category, const QString &name, QObject *parent);
     static NotificationSoundId soundIdFromString(const QString &category, const QString &name);
 
-  	LNotificationSound(NotificationSoundId id, QObject *parent);
-  	LNotificationSound(const QString &id, QObject *parent);
+    LNotificationSound(NotificationSoundId id, QObject *parent);
+    LNotificationSound(const QString &id, QObject *parent);
     bool isPlaying();
     bool isValid();
     const QString *name();
@@ -34,7 +41,7 @@ signals:
     void stopped();
 
 public slots:
-	  void play(int repeatCount);
+    void play(int repeatCount);
     void stop();
     void seek(float duration);
 
